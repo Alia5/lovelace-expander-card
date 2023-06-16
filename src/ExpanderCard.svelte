4a718e6f47b185d6ed554e223e4f657c1a9c78c0
Christos Kourouniades <christoskourouniades@Christoss-MBP.localdomain>
2023-06-16 12:16:55 +0300

    fixed bug

--- src/ExpanderCard.svelte
+++ src/ExpanderCard.svelte
@@ -168,7 +168,7 @@
         text-align: left;
     }
     .ico {
-        color: white;
+        color: var(--primary-text-color);
         transition-property: transform;
         transition-duration: 0.35s;
     }
