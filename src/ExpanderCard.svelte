9fd7427990450b16043e06e79ffaf0e4b85bd9db
Dennis Melzer <melzer.dennis@gmail.com>
2024-05-12 22:32:02 +0200

    Fix layout and gap

--- src/ExpanderCard.svelte
+++ src/ExpanderCard.svelte
@@ -102,7 +102,7 @@
     {/if}
     {#if config.cards}
         <div
-            style="--gap:{config.gap}; --child-padding:{config['child-padding']}"
+            style="--gap {open ? config['expanded-gap'] : config.gap}; --child-padding:{config['child-padding']}"
             class="children-container"
             use:collapse={{ open }}
         >
